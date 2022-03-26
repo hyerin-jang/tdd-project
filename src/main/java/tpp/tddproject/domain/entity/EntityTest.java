09dc34400a83d607e2a189a92e4676b106072eff
hyechan <dhgpcks@gmail.com>
2022-03-26 16:17:02 +0900

    p6spy, h2db 인메모리 연결

--- src/main/java/tpp/tddproject/domain/entity/EntityTest.java
+++ src/main/java/tpp/tddproject/domain/entity/EntityTest.java
@@ -1,4 +1,16 @@
 package tpp.tddproject.domain.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class EntityTest {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 }
