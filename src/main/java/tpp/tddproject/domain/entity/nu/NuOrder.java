5c7feeb69122b1962b93f9a817fe29040f58755c
hodolee <hodolee@matey.co.kr>
2022-03-31 12:42:42 +0900

    이게 이렇게 하는게 마맞는지..?

--- src/main/java/tpp/tddproject/domain/entity/nu/NuOrder.java
+++ src/main/java/tpp/tddproject/domain/entity/nu/NuOrder.java
@@ -3,6 +3,9 @@ package tpp.tddproject.domain.entity.nu;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class NuOrder {
@@ -12,5 +15,14 @@ public class NuOrder {
 
     @Column(nullable = false)
     private String nuOrderDate;
+
+    @OneToMany(mappedBy = "nu_delivery")
+    private List<NuDelivery> nuDeliveries;
+
+    @OneToMany(mappedBy = "nu_order_item")
+    private List<NuOrderItem> nuOrderItems;
+
+    @OneToMany(mappedBy = "nu_refund")
+    private List<NuRefund> nuRefunds = new ArrayList<>();
 
 }
