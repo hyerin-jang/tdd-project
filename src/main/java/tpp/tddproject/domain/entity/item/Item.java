c78755cc4026ce8c1702bd49a9599dd1462c9be0
hyerin-jang <hrjang92@gmail.com>
2022-04-09 20:09:37 +0900

    init item repository test

--- src/main/java/tpp/tddproject/domain/entity/item/Item.java
+++ src/main/java/tpp/tddproject/domain/entity/item/Item.java
@@ -1,11 +1,17 @@
 package tpp.tddproject.domain.entity.item;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import tpp.tddproject.domain.entity.nu.NuOrderItem;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
 
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -30,6 +36,20 @@ public class Item {
     private String itemStatus;
 
     @OneToMany(mappedBy = "nuOrderItemNo")
-    private List<NuOrderItem> nuOrderItems;
+    private List<NuOrderItem> nuOrderItems = new ArrayList<>();
+
+    @Builder
+    public Item (String itemComp, String itemName, int itemPrice, int itemStock,
+                String itemDesc, int itemSale, String itemStatus) {
+
+        this.itemComp = itemComp;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemStock = itemStock;
+        this.itemDesc = itemDesc;
+        this.itemSale = itemSale;
+        this.itemStatus = itemStatus;
+
+    }
 
 }
