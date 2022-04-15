1e9fbb56c33e131d7e93b826f2b0cc109fe635e7
hyerin-jang <hrjang92@gmail.com>
2022-04-16 03:59:03 +0900

    init ItemController unit test

---
+++ src/test/java/tpp/tddproject/rin/controller/ItemControllerUT.java
@@ -0,0 +1,153 @@
+package tpp.tddproject.rin.controller;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import tpp.tddproject.rin.dto.ItemDto;
+import tpp.tddproject.rin.service.ItemService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(ItemController.class)
+//@Controller, @ControllerAdvice 사용 가능
+//@Service, @Repository 등은 사용 불가
+//@MockBean 으로 가짜 객체 등록해서 사용해야 함
+//JPA 기능은 동작 x
+public class ItemControllerUT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ItemService itemService;
+
+    List<ItemDto> itemDtoList;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("@BeforeAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        itemDtoList = Arrays.asList(ItemDto.builder()
+                .itemNo(1L)
+                .itemComp("nike")
+                .itemName("hoodie")
+                .itemPrice(10000)
+                .itemStock(24).build());
+
+        System.out.println("@BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("@AfterEach");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("@AfterAll");
+    }
+
+    @Test
+    public void findAllItemTest() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/item"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Test
+    public void findItemByIdTest() throws Exception {
+        //given
+        List<ItemDto> itemDtoList = this.itemDtoList;
+        given(itemService.findItemByItemNo(any())).willReturn(itemDtoList);
+
+        //when & then
+        Long itemNo = itemDtoList.get(0).getItemNo();
+        mockMvc.perform(MockMvcRequestBuilders.get("/item/" + itemNo))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$..itemNo").value(1))
+                .andExpect(jsonPath("$..itemComp").value("nike"))
+                .andExpect(jsonPath("$..itemName").value("hoodie"))
+                .andExpect(jsonPath("$..itemPrice").value(10000))
+                .andExpect(jsonPath("$..itemStock").value(24))
+                .andDo(print());
+    }
+
+    @Test
+    void addItemTest() throws Exception {
+        //given
+        List<ItemDto> itemDtoList = this.itemDtoList;
+        willDoNothing().given(itemService).addItem(any());
+
+        //when & then
+        mockMvc.perform(post("/item")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(
+                                "{ \"itemNo\" : 1, " +
+                                        "\"itemComp\" : \"polo\", " +
+                                        "\"itemName\" : \"knit\", " +
+                                        "\"itemPrice\" : 30000, " +
+                                        "\"itemStock\" : 32}"
+                        ))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void updateItemTest() throws Exception {
+        //given
+        List<ItemDto> itemDtoList = this.itemDtoList;
+        willDoNothing().given(itemService).updateItem(any());
+
+        //when & then
+        Long itemNo = itemDtoList.get(0).getItemNo();
+
+        mockMvc.perform(put("/item/" + itemNo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(
+                                "{ \"itemNo\" : 1, " +
+                                    "\"itemComp\" : \"polo\", " +
+                                    "\"itemName\" : \"knit\", " +
+                                    "\"itemPrice\" : 30000, " +
+                                    "\"itemStock\" : 32}"
+                        ))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    void testDeleteContent() throws Exception {
+        //given
+        List<ItemDto> itemDtoList = this.itemDtoList;
+        willDoNothing().given(itemService).deleteItem(any());
+
+        //when & then
+        Long itemNo = itemDtoList.get(0).getItemNo();
+
+        mockMvc.perform(delete("/item/" + itemNo))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
