0968df48cb857ddb7de1ee779f246c10e2137bcd
hyechan <dhgpcks@gmail.com>
2022-04-30 16:14:02 +0900

    Feat : 주소록 단건조회 - 컨트롤러 단위테스트

--- src/main/java/tdd/tddproject/hyechan/controller/AddressController.java
+++ src/main/java/tdd/tddproject/hyechan/controller/AddressController.java
@@ -1,17 +1,23 @@
 package tdd.tddproject.hyechan.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import tdd.tddproject.hyechan.service.AddressService;
 
 import static tdd.tddproject.global.util.Util.getMap;
 
 @RestController
+@RequiredArgsConstructor
 public class AddressController {
+
+    private final AddressService addressService;
 
     @GetMapping("/address/{id}")
     public ResponseEntity<?> getById(@PathVariable Long id){
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok()
+                .body(getMap(addressService.getById(id)));
     }
 }
---
+++ src/test/java/tdd/tddproject/hyechan/controller/AddressControllerUnitTest.java
@@ -0,0 +1,55 @@
+package tdd.tddproject.hyechan.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import tdd.tddproject.domain.entity.user.Address;
+import tdd.tddproject.hyechan.mapper.AddressMapper;
+import tdd.tddproject.hyechan.service.AddressService;
+import tdd.tddproject.hyechan.util.AddressConstructor;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AddressController.class)
+public class AddressControllerUnitTest extends AddressConstructor {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AddressService addressService;
+
+    AddressMapper mapper = Mappers.getMapper(AddressMapper.class);
+
+    // @author: hyechan, @since: 2022/04/30 3:40 오후
+    @Test
+    void 주소록_단건_조회_성공() throws Exception{
+        //given
+        Long id = 1L;
+        Address address = createEntity(createParam());
+        given(addressService.getById(id)).willReturn(mapper.toDto(address));
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/address/{id}", id)
+                .accept(MediaType.APPLICATION_JSON_VALUE));
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.addressId").value(id))
+                .andExpect(jsonPath("$.result.addressCity").value(ADDRESS_CITY))
+                .andExpect(jsonPath("$.result.addressZip").value(ADDRESS_ZIP))
+                .andExpect(jsonPath("$.result.addressPhone").value(ADDRESS_PHONE))
+                .andExpect(jsonPath("$.result.addressReceiver").value(ADDRESS_RECEIVER))
+                .andExpect(jsonPath("$.result.addressStreet").value(ADDRESS_STREET))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+}
